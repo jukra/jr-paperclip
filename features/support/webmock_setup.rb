f73ac8b854c67efcf2bfb188fa4af9ba121c868e
Jukka Rautanen <jukka@rautanen.info>
2025-12-16 14:47:18 +0200

    Fix WebMock configuration for AWS SDK credential detection
    - Stub AWS EC2 metadata service endpoints in Before hook
    - Prevents WebMock::NetConnectNotAllowedError in S3 tests
    - AWS SDK tries to detect credentials from EC2 metadata in CI environment

--- features/support/webmock_setup.rb
+++ features/support/webmock_setup.rb
@@ -1,3 +1,8 @@
 require "webmock/cucumber"
 
-WebMock.disable_net_connect!(allow_localhost: true)
+# Allow AWS EC2 instance metadata service (used by AWS SDK for credential detection)
+# This IP is a standard link-local address used by all major cloud providers
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: "169.254.169.254"
+)
